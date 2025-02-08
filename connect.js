350a4badfe598769aeda04e3703a9d6e318f347e
laveup <kapitanenkoandrej464@gmail.com>
2025-02-09 00:06:03 +0300

    Add files via upload

--- connect.js
+++ connect.js
@@ -1,24 +1,24 @@
-const tg = window.Telegram.WebApp;
+//const tg = window.Telegram.WebApp;
 
 // Инициализация
-tg.ready();
+//tg.ready();
 
 // Получаем данные пользователя
-const user = tg.initDataUnsafe.user;
-if (user) {
-  document.getElementById('username').innerText = user.first_name || 'User';
-} else {
-  document.getElementById('username').innerText = 'Guest';
-}
+//const user = tg.initDataUnsafe.user;
+//if (user) {
+  //document.getElementById('username').innerText = user.first_name || 'User';
+//} else {
+  //document.getElementById('username').innerText = 'Guest';
+//}
 
 // Отправка данных
-document.getElementById('send-data').addEventListener('click', () => {
-  tg.sendData(JSON.stringify({ message: "Hello from Mini App!" }));
-  tg.close();
-});
-
+//document.getElementById('send-data').addEventListener('click', () => {
+  //tg.sendData(JSON.stringify({ message: "Hello from Mini App!" }));
+  //tg.close();
+//});
+//
 // Кнопка "Назад"
-tg.BackButton.show();
-tg.BackButton.onClick(() => {
-  tg.close();
-});
+//tg.BackButton.show();
+//tg.BackButton.onClick(() => {
+//  tg.close();
+//});
