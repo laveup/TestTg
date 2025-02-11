f62de5f1344cddd1f2a1f4b08bfb149832eca859
laveup <kapitanenkoandrej464@gmail.com>
2025-02-11 22:54:31 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -16,40 +16,36 @@ function getScore() {
 
 function addOne() {
     const currentScore = getScore();
-    const increment = currentScore >= 50 ? 2 : 1; // Если счет >= 50, добавляем 2, иначе 1
-    const increment2 = currentScore >= 100 ? 4 : 1;
-    const increment3 = currentScore >= 1000 ? 1000000 : 1;
+    let increment = 1; // По умолчанию +1
+
+    if (currentScore >= 200) {
+        increment = 1000; // Если счет >= 1000, добавляем +1000
+    } else if (currentScore >= 100) {
+        increment = 4; // Если счет >= 100, добавляем +4
+    } else if (currentScore >= 50) {
+        increment = 2; // Если счет >= 50, добавляем +2
+    }
+
     setScore(currentScore + increment);
-    setScore(currentScore + increment2);
-    setScore(currentScore + increment3);
 }
 
 $circle.addEventListener('click', (event) => {
-    const rect = $circle.getBoundingClientRect();
-    const offsetX = event.clientX - rect.left - rect.width / 2;
-    const offsetY = event.clientY - rect.top -  rect.height / 2;
-
-    const DEG = 40;
-
-    const tiltX = (offsetY / rect.height) * DEG;
-    const tiltY = (offsetX / rect.width) * -DEG;
-
-    $circle.style.setProperty('--tiltX', `${tiltX}deg`);
-    $circle.style.setProperty('--tiltY', `${tiltY}deg`);
-
-    setTimeout(() => {
-        $circle.style.setProperty('--tiltX', `0deg`);
-        $circle.style.setProperty('--tiltY', `0deg`);
-    }, 150);
     console.log('Click');
-    
+    const rect = $circle.getBoundingClientRect();
     const currentScore = getScore();
-    const increment = currentScore >= 50 ? 2 : 1;
-    const increment2 = currentScore >= 100 ? 4 : 1;
-    const increment3 = currentScore >= 1000 ? 1000000 : 1;
+    let increment = 1;
+
+    if (currentScore >= 200) {
+        increment = 1000;
+    } else if (currentScore >= 100) {
+        increment = 4;
+    } else if (currentScore >= 50) {
+        increment = 2;
+    }
+
     const plusOne = document.createElement('div');
     plusOne.classList.add('plus-one');
-    plusOne.textContent = `+${increment, increment2, increment3}`;
+    plusOne.textContent = `+${increment}`; // Отображаем +1, +2, +4 или +1000
     plusOne.style.left = `${event.clientX - rect.left}px`;
     plusOne.style.top = `${event.clientY - rect.top}px`;
     $circle.parentElement.appendChild(plusOne);
